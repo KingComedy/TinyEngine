c00befe887c24a4e0d4ff74f79e42a474be100e4
wuzhun.wz <wuzhun.wz@alibaba-inc.com>
2018-09-29 19:39:47 +0800

    update demo-miio-scene program and document

--- scripts/demos/demo-miio-scene/index.js
+++ scripts/demos/demo-miio-scene/index.js
@@ -3,8 +3,15 @@ console.log("         demo-miio-scene         ");
 console.log("=================================");
 
 // miio 为 Natvie Modules，按照小米设备接入协议实现的扩展对象
-
+// 如下设备的Did，token需要填入真实设备的信息，具体获取方法请参考README
 var lightDevice;
+var xiaoMiMotionSentorDid = '158d000208e44a';  //小米人体感应器的deviceID
+var xiaoMiGatewayDid = 87383843;   //小米网关的deviceID
+var xiaoMiLightDid = 61632282;   //小米WIFI灯的deviceID
+var xiaoMiGatewayToken = "2e7605a92dedd89171a9aa761ba74adb";  //小米网关的Token
+var xiaoMiLightToken = "ae65d127852df5c2b86f38b6878e8706";  //小米WIFI灯的Token
+var WiFiSSID = 'xxx'; //小米设备连接的WiFi热点名称
+var WiFiPasswd = 'xxxx'; //小米设备连接的WiFi热点密码
 
 // 控制开灯
 function lightOn() {
@@ -24,10 +31,10 @@ function lightOff() {
 
 function setupLight(host) {
   // 创建小米灯设备，如何获取小米设备的token，
-  //其具体方法和步骤请见：https://lark.alipay.com/tinyenginedev/developer-documentation/wbm6d5
+  //其具体方法和步骤请见：
   lightDevice = miio.createDevice(
     host,
-    "ae65d127852df5c2b86f38b6878e8706"
+    xiaoMiLightToken
   );
 
   // 初次启动时，第一次关闭小米灯
@@ -39,30 +46,33 @@ function setupGateway(host) {
   //其具体方法和步骤请见：https://lark.alipay.com/tinyenginedev/developer-documentation/wbm6d5
   var gatewayDevice = miio.createDevice(
     host,
-    "0a7b7e5af0fc0b1b4877b0907834f9a7"
+    xiaoMiGatewayToken
   );
 
   var motionSensorId = null;
   var timeout = null;
   // 注册监听事件，回调函数
-  miio.deviceOnEvent(gatewayDevice, function(event) {
-    console.log("gateway receive event");
+  miio.deviceOnEvent(gatewayDevice, function (event) {
+    console.log("gateway receive event:"+event);
 
     var obj = JSON.parse(event);
+
     var open = false;
 
     if (obj.data) {
       open = JSON.parse(obj.data).status === "motion";
     }
+
+    console.log('hellosid'+obj.sid);
     //如果人体感应器检测到人，则开灯，如果一分钟没有感应到人存在，则关灯
-    if (obj.cmd === "report" && obj.sid === motionSensorId && open) {
+    if (obj.cmd === "report" && obj.sid === xiaoMiMotionSentorDid && open) {
       lightOn();
       if (timeout) {
         clearTimeout(timeout);
         timeout = null;
       }
       //每分钟接收一次
-      timeout = setTimeout(function() {
+      timeout = setTimeout(function () {
         lightOff();
       }, 60 * 1000);
     }
@@ -75,23 +85,19 @@ function setupGateway(host) {
     '["lumi.0","device_list"]'
   );
   console.log("device list: " + device_list);
-
-  // 从小米网关下挂的设备列表中，找到人体感应设备，网关下的其他设备可以按照类似的方法
-  var deviceList = JSON.parse(device_list);
-  for (var i = 0; i < deviceList.result.length; i++) {
-    if (deviceList.result[i + 1] === 2) {
-      motionSensorId = deviceList.result[i].substring(5);
-      console.log("motion sensor id: " + motionSensorId);
-      break;
-    }
-  }
 }
 
-miio.discover(20, function(host, deviceId) {
-  console.log('discovered device, host: ' + host + ', deviceId: ' + deviceId);
-  if (deviceId === 61632282) {
-    setupLight(host);
-  } else if (deviceId === 78463041) {
-    setupGateway(host);
-  }
+WIFI.connect(WiFiSSID, WiFiPasswd,function(){
+  var ip = WIFI.getip();
+  console.log('ip=', ip);
+  miio.discover(20, function (host, deviceId) {
+    console.log('discovered device, host: ' + host + ', deviceId: ' + deviceId);
+    if (deviceId === xiaoMiLightDid) {
+      setupLight(host);
+    } else if (deviceId === xiaoMiGatewayDid) {
+      setupGateway(host);
+    }
+  });
 });
+
+
